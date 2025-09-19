4f7ef219b58fc6c6c882eb128da6319ed508322b
Grant Hung <a71526sea@gmail.com>
2025-09-19 19:06:18 +0800

    force push

--- src/main/java/com/babymate/forum/controller/ForumController.java
+++ src/main/java/com/babymate/forum/controller/ForumController.java
@@ -63,7 +63,7 @@ public class ForumController {
         postService.updatePost(id, post);  // 交給 service
         return "redirect:/forum/post/" + id;
     }
- // 軟刪除文章22222
+ // 軟刪除文章22222333323
     @GetMapping("/post/delete/{id}")
     public String deletePost(@PathVariable Integer id) {
         postService.softDeletePost(id);  // 交給 service
