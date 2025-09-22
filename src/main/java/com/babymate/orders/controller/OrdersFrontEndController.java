56f2ec6f523f70db19727ccb8d543cf6c0c89ce6
yuchihwei <skywei780722@gmail.com>
2025-09-22 23:39:15 +0800

    購物車結帳連結新增、訂單資料及結帳頁面製作 1.購物車結帳連結至shop-checkout.html 2.新增前台訂單明細，以登入會員後，顯示該會員訂單資料(有新增OrdersFrontEndController.java；內容有新增:ordersRepository.java、OrderService.java、OrderVO.java) 3.新增CartToCheckoutController.java，用於訂單結帳頁抓取Redis內購物車明細。

---
+++ src/main/java/com/babymate/orders/controller/OrdersFrontEndController.java
@@ -0,0 +1,40 @@
+package com.babymate.orders.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.babymate.member.model.MemberVO;
+import com.babymate.orderDetail.model.OrderDetailService;
+import com.babymate.orders.model.OrdersService;
+import com.babymate.orders.model.OrdersVO;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/my-orders")
+public class OrdersFrontEndController {
+	
+	@Autowired
+	private OrderDetailService otderDetailSvc;
+	
+	@Autowired
+	private OrdersService ordersSvc;
+	
+	@GetMapping
+	public String myOrders(HttpSession session, ModelMap model) {
+		MemberVO member = (MemberVO) session.getAttribute("member");
+		if (member == null) {
+			return "redirect:/shop/login";
+		}
+		
+		List<OrdersVO> myOrders = ordersSvc.getOrdersByMemberId(member.getMemberId());
+		model.addAttribute("orders", myOrders);
+		return "frontend/my-orders";
+	}
+
+}
