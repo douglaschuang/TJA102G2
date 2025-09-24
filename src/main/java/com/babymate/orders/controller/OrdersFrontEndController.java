4883b863f0a62cfa1ee6f89d0f3b9303e54229f2
yuchihwei <skywei780722@gmail.com>
2025-09-24 12:56:36 +0800

    新增FAQ前台內容、前台商城內容調整、新增前台訂單明細

--- src/main/java/com/babymate/orders/controller/OrdersFrontEndController.java
+++ src/main/java/com/babymate/orders/controller/OrdersFrontEndController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.babymate.member.model.MemberVO;
 import com.babymate.orderDetail.model.OrderDetailService;
+import com.babymate.orderDetail.model.OrderDetailVO;
 import com.babymate.orders.model.OrdersService;
 import com.babymate.orders.model.OrdersVO;
 
@@ -20,7 +23,7 @@ import jakarta.servlet.http.HttpSession;
 public class OrdersFrontEndController {
 	
 	@Autowired
-	private OrderDetailService otderDetailSvc;
+	private OrderDetailService orderDetailSvc;
 	
 	@Autowired
 	private OrdersService ordersSvc;
