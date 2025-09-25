a4131b3cfa8f8f8b954cd12d3b429e7ca8a71794
Douglas <douglas.chuang@gmail.com>
2025-09-25 16:04:08 +0800

    後台管理介面新增RBAC功能(角色存取控制)
    1. 目前預設三個角色: 系統管理員, 客服代表 , 商城管理員.根據不同角色登入會有不同的menu選單及功能權限, 無權限會顯示403頁面
    (error/403).
    2. 調整首頁url從/admin -> /admin/dashboard (對應呼叫的部分也一併修改)
    3. 登入檢核url從/staff/loginCheck -> /staff/checkin/loginCheck
    4. 新增權限過濾器(StaffPermissionFilter)檢核權限

--- src/main/java/com/babymate/config/WebConfig.java
+++ src/main/java/com/babymate/config/WebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.babymate.staff.filter.StaffLoginFilter;
+import com.babymate.staff.filter.StaffPermissionFilter;
 
 @Configuration
 public class WebConfig {
@@ -16,7 +17,21 @@ public class WebConfig {
         registrationBean.setFilter(new StaffLoginFilter());
 
         // 攔截 /admin/** 的所有請求（排除 login 自行處理）
-        registrationBean.addUrlPatterns("/admin/*");
+        registrationBean.addUrlPatterns("/admin/*", "/pregnancy/*", "/mhb/*", "/staff/*");
+        registrationBean.setOrder(1); // 先執行
+
+        return registrationBean;
+    }
+    
+    @Bean
+    public FilterRegistrationBean<StaffPermissionFilter> staffPermissionFilter() {
+    	
+        FilterRegistrationBean<StaffPermissionFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new StaffPermissionFilter());
+
+        // 攔截 /admin/** 的所有請求（排除 login 自行處理）
+        registrationBean.addUrlPatterns("/admin/*", "/pregnancy/*", "/mhb/*", "/staff/*");
+        registrationBean.setOrder(2);
 
         return registrationBean;
     }
